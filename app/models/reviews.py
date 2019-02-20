c17ed073f1a0c9694267ed06f44aadf0e9214dd8
mukasine marie claire <mclaremukasine@gmail.com>
2019-02-20 11:52:15 +0200

    change attributes of review class,add macro to display articles

--- app/models/reviews.py
+++ app/models/reviews.py
@@ -1,17 +1,15 @@
 class Review:
 
     all_reviews = []
-    def __init__(self,news_id,title,imageurl,review):
-        self.news_id = news_id
-        self.title = title
+    def __init__(self,author,title,description,url,imageUrl,publishedAt):
+        self.author = author
+        self.title=title
+        self.description= description
+        self.url=url
         self.imageurl = imageurl
-        self.review = review
+        self.publishedAt = publishedAt
 
 
-    def save_review(self):
-        Review.all_reviews.append(self)
+   
 
-
-    @classmethod
-    def clear_reviews(cls):
-        Review.all_reviews.clear()
+    
